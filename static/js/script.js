780e47c842153444652452e3247c507cecdc1fb3
Shashank Shekhar <ssshekhu53@gmail.com>
2021-02-23 12:03:25 +0530

    Added copy feature

--- static/js/script.js
+++ static/js/script.js
@@ -9,9 +9,6 @@ $('#short-form').validate({
         urlField: "Please enter a URL"
     },
     submitHandler: function (form) {
-        $('.alert').fadeOut();
-        $(form).find('button').attr('disabled', true);
-        $(form).find('button').text('Tinyfying...');
         let fd=new FormData(form);
         $.ajax({
            type: 'POST',
@@ -20,13 +17,33 @@ $('#short-form').validate({
            contentType: false,
            processData: false,
            dataType: 'json',
+           beforeSend: () => {
+               $('.alert').fadeOut();
+               $(form).find('button:not(#copy-btn)').attr('disabled', true);
+               $(form).find('button:not(#copy-btn)').text('Tinyfying...');
+               $('.alert input[name="short_url"]').val('');
+               $('.alert').hide();
+               $('#copy-btn').html('<i class="far fa-copy"></i>Copy');
+               $('#copy-btn').removeClass('btn-success');
+               $('#copy-btn').addClass('btn-danger');
+               $('#copy-btn').hide();
+           },
            success: function (response) {
                console.log(response);
-               $('.alert').text(response.short_url);
+               $('.alert input[name="short-url"]').attr('value', response.short_url);
                $('.alert').fadeIn();
-               $(form).find('button').text('Make TinyURL!');
-               $(form).find('button').attr('disabled', false);
+               $(form).find('button:not(#copy-btn)').text('Make TinyURL!');
+               $(form).find('button:not(#copy-btn)').attr('disabled', false);
+               $('#copy-btn').show();
            }
         });
     }
+});
+
+$(document).on('click', '#copy-btn', function(){
+    $(`input[name="short-url"]`).select();
+    document.execCommand("copy");
+    $(this).html('<i class="fas fa-check"></i>Copied');
+    $(this).removeClass('btn-danger');
+    $(this).addClass('btn-success');
 });
